eb9e925951483447d114a3925c7306d762f62a19
nakul <nakul@PF2Y2GJC.>
2024-03-07 11:55:28 -0800

    base code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use thirtyfour::prelude::*;
+
+async fn initialize_driver() -> WebDriverResult<()> {
+    let caps = DesiredCapabilities::chrome();
+    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+    driver.maximize_window().await?;
+    driver.goto("https://www.google.com").await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> WebDriverResult<()> {
+    initialize_driver().await?;
+    Ok(())
 }
